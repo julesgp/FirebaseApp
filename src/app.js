f15d21d301b181ffacd25e711fae3bf04baac932
Julian Garcia <juliangarcia@Julians-MacBook-Pro.local>
2019-06-18 21:00:45 -0400

    added firebase

--- src/app.js
+++ src/app.js
@@ -1,27 +1,14 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import HeaderMenu from 'HeaderMenu.js';
-
+import LogInForm from './components/logInForm';
+import firebase from './firebase';
 
 class App extends Component {
-	constructor(){
-		super();
-		this.state()={
-			displayHeaderMenu:true,
-		};
+	render(){
+		return (
+			<LogInForm />
+		)
 	}
 
-    render(){
-    	let displayHeader= "";
-    	if(this.state.displayHeaderMenu === true){
-    		showGame = (
-    			<HeaderMenu />
-    		)
-    	}
-
-    	return (
-    	    {showGame}
-    	)
-    }
 }
 ReactDOM.render(<App />, document.getElementById('app'));
