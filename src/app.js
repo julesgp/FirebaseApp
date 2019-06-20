63388e30225b15e4a5608cfc5c0fcdd0ff3709d1
Julian Garcia <juliangarcia@Julians-MacBook-Pro.local>
2019-06-19 20:07:38 -0400

    updated app.js file

--- src/app.js
+++ src/app.js
@@ -1,14 +1,42 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import LogInForm from './components/logInForm';
 import firebase from './firebase';
 
 class App extends Component {
-	render(){
-		return (
-			<LogInForm />
-		)
+	constructor(){
+		super();
+		this.state = {
+			books: [],
+			userInput: ''
+		};
+		this.handleChange = this.handleChange.bind(this);
 	}
 
+	handleChange(event){
+		this.setState({userInput: event.target.value});
+		console.log("it is changing now")
+	}
+
+	handleClick() {
+		const dbRef = firebase.database().ref();
+		dbRef.push(this.state.userInput);
+		this.setState({ userInput: "" })
+	}
+	
+	render(){
+		return (
+			<div>
+				<ul>
+					{this.state.books.map((book, i) => {
+						return(
+							<li key={i}>{book}</li>
+						)
+					})}
+				</ul>
+				<input type="text" onChange={this.handleChange} placeholder="add book here" />
+				<button>Add book</button>
+			</div>
+		)
+	}
 }
 ReactDOM.render(<App />, document.getElementById('app'));
